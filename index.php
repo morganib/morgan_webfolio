ae142d48c1dbc56c3b263cffca8b7da4f717980a
morganib <morgan.contact@yahoo.com>
2019-05-02 09:30:33 +0200

    pasted

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+	<html>
+
+			<head>
+				<title>Webfolio morgan</title>
+				<meta charset="utf-8"/>
+				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+				<meta name="description" content="Webfolio de morgan blanc"/>
+				<meta name="keywords" content="webfolio, morgan, blanc, graphiste, designer"/>
+				<meta http-equiv="cache-control" content="no-cache"/>
+				<link rel="stylesheet" type="text/css" href="css/global.css" />
+			</head>
+
+			<body>
+				<?php $active = "webfolio_morgan"; ?>
+        <div class="page page__home">
+          hhhh
+
+
+					<main class="page--content">
+
+
+
+          </main>
+          <!-- /page--content -->
+
+				<!-- <?php include("partials/page--foot.php"); ?> -->
+
+        </div>
+        <!-- /page -->
+
+
+			</body>
+	</html>
